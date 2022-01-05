bc1b74914346ac89baa077011a7c521e728bbe5c
Jennifer Roques <jenniferroques@gmail.com>
2022-01-05 17:32:11 -0600

    commit tree 88745281c2a44841363ca359f1a555b906253b64

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
-}
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-']+/g, " ");
+  }
+
+  static titleize(string) {
+    let array = string.split(' ');
+    let newarray = [];
+
+    for (let x = 0; x < array.length; x++) {
+      newarray.push(array[x].charAt(0).toUpperCase() + array[x].slice(1));
+    }
+    return newarray.join(' ');
+  }
+} 
